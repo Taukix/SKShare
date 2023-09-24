21999c0b72afb63754a564c70044c442f760aa8a
tommy <tommyboisseau03@gmail.com>
2023-09-24 15:24:20 +0200

    Fix (Likes & Dislikes) Change color

--- resources/views/sneakers/edit.blade.php
+++ resources/views/sneakers/edit.blade.php
@@ -46,7 +46,7 @@
             </form>
         </div>
     </div>
-    <a href="{{ route('sneakers') }}">
+    <a href="{{ route('sneakers.show', $sneaker->id) }}">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-white hover:fill-black w-6 h-6 ml-8 rounded-full hover:bg-amber-200 p-1 mt-8 mb-6 w-8 h-8">
             <path fill-rule="evenodd" d="M9.53 2.47a.75.75 0 010 1.06L4.81 8.25H15a6.75 6.75 0 010 13.5h-3a.75.75 0 010-1.5h3a5.25 5.25 0 100-10.5H4.81l4.72 4.72a.75.75 0 11-1.06 1.06l-6-6a.75.75 0 010-1.06l6-6a.75.75 0 011.06 0z" clip-rule="evenodd" />
         </svg>
